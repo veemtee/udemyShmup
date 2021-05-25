fe4bc5c1a5c6f0a2f4d4269049e6a9e9f2596f27
veemtee <veemtee@protonmail.com>
2021-05-25 14:42:14 +0300

    kouluiltissave

--- Assets/EarthRootMover.cs
+++ Assets/EarthRootMover.cs
@@ -13,6 +13,6 @@ public class EarthRootMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        transform.Translate(Vector3.forward * 0.55f * Time.deltaTime);
     }
 }
